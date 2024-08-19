69eca08941da35833306e51f164c50e2b8b467d0
Renan S Dechen <123004957+RenanSDechen@users.noreply.github.com>
2024-08-19 14:17:30 +0000

    resolve flex problem

--- version_1/src/app/page.tsx
+++ version_1/src/app/page.tsx
@@ -1,12 +1,12 @@
 
 export default function Home() {
   return (
-    <main className="grid  grid-cols-2 place-content-start w-screen ">
-      <div className="w-1/3 h-full">Menu</div>
-      <div className="bg-slate-500 grid grid-cols-3 gap-4 h-2/3 w-2/3">
-         <div className="py-4 w-auto">01</div>
-         <div className="py-12 w-auto">02</div>
-         <div className="py-8  w-auto">03</div>
+    <main className="flex justify-start w-screen hover:justify-between ">
+      <div className="w-2/12 h-full bg-slate-900 ">Menu</div>
+      <div className="grid grid-cols-3 gap-4 h-screen w-full p-4">
+         <div className="w-auto bg-slate-500 rounded-lg">01</div>
+         <div className="w-auto bg-slate-500 rounded-lg">02</div>
+         <div className="w-auto bg-slate-500 rounded-lg">03</div>
       </div>
     </main>
   );
