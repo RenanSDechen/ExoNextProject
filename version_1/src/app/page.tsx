c87bb25c39e5d05101fed659eec3673a892488fb
Renan S Dechen <123004957+RenanSDechen@users.noreply.github.com>
2024-08-19 11:46:13 +0000

    commit.1.3

---
+++ version_1/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <main className="flex  size-full h-screen w-screen aligns bg-white ">
+      <div className="bg-slate-500 grid grid-cols-3 gap-4 h-2/3 w-2/3">
+         <div className="py-4 w-auto">01</div>
+         <div className="py-12 w-auto">02</div>
+         <div className="py-8  w-auto">03</div>
+      </div>
+    </main>
+  );
+}
