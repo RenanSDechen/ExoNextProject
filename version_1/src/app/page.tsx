9e435d251f8befa19949277655f5eb32dd9b12fd
Renan S Dechen <123004957+RenanSDechen@users.noreply.github.com>
2024-08-19 12:07:28 +0000

    commit.1.4

--- version_1/src/app/page.tsx
+++ version_1/src/app/page.tsx
@@ -1,7 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex  size-full h-screen w-screen aligns bg-white ">
+    <main className="grid  grid-cols-2 place-content-start w-screen ">
+      <div className="w-1/3 h-full">Menu</div>
       <div className="bg-slate-500 grid grid-cols-3 gap-4 h-2/3 w-2/3">
          <div className="py-4 w-auto">01</div>
          <div className="py-12 w-auto">02</div>
