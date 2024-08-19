23ac1882de32275dd1953792fe6812a4d943a78c
Renan S Dechen <123004957+RenanSDechen@users.noreply.github.com>
2024-08-19 14:25:47 +0000

    resolve column task

--- version_1/src/app/page.tsx
+++ version_1/src/app/page.tsx
@@ -3,10 +3,26 @@ export default function Home() {
   return (
     <main className="flex justify-start w-screen hover:justify-between ">
       <div className="w-2/12 h-full bg-slate-900 ">Menu</div>
-      <div className="grid grid-cols-3 gap-4 h-screen w-full p-4">
-         <div className="w-auto bg-slate-500 rounded-lg">01</div>
-         <div className="w-auto bg-slate-500 rounded-lg">02</div>
-         <div className="w-auto bg-slate-500 rounded-lg">03</div>
+      <div className="grid grid-cols-4 gap-4 h-screen w-full p-4">
+         <div className="w-auto bg-slate-500 rounded-lg p-4">
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+         </div>
+         <div className="w-auto bg-slate-500 rounded-lg p-4">
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+         </div>
+         <div className="w-auto bg-slate-500 rounded-lg p-4">
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+         </div>
+         <div className="w-auto bg-slate-500 rounded-lg p-4">
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+          <div className="w-full rounded-lg h-40 bg-slate-200 my-2 text-black ">TOME</div>
+         </div>
       </div>
     </main>
   );
